8d9191ef23bc63fe061e002650388135ab110bd1
inanc dagyolu <inancdagyolu@inancs-Air.home>
2023-11-05 14:44:04 +0300

    Database

--- marjikon/settings/production.py
+++ marjikon/settings/production.py
@@ -1,5 +1,20 @@
 from .base import *
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 ALLOWED_HOSTS = ['marjikon.com', 'www.marjikon.com']
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '',
+    }
+}
